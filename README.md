0ddcd88b508852299406cf48af49479775c21a04
Phillip Klinefelter <alphageek83@gmail.com>
2011-08-02 19:08:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Cloudy Hacker News
+==================
+
+A Chrome extension to weight the links on Hacker News based on points.
---
+++ cloudyhn.js
@@ -0,0 +1,12 @@
+var max = 300;
+var divisionUnit=max/10;
+var scores = document.querySelectorAll(".subtext span");
+for (var i = 0; i < scores.length; i++) {
+    score = scores[i].textContent.split(' ')[0];
+    scores[i].style.fontWeight = 'bold';
+    scores[i].style.fontSize = '8pt';
+    if (score > max) { score = max; }
+    var fontUnit = Math.round(score/divisionUnit);
+    var title = scores[i].parentNode.parentNode.previousSibling.querySelectorAll("td.title a")[0];
+    title.style.fontSize = (10+(fontUnit*3)).toString()+'pt';
+}
