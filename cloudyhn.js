975f7c4668a45414fc95a83f49d0d9790c4a96ae
Phil Klinefelter <phillip.klinefelter@gmail.com>
2016-08-17 23:28:37 -0700

    Handle stories without scores

--- cloudyhn.js
+++ cloudyhn.js
@@ -1,14 +1,15 @@
 var max = 300;
 var divisionUnit = max / 10;
-var scores = document.querySelectorAll(".score");
+var subtexts = document.querySelectorAll(".subtext");
 var titles = document.querySelectorAll(".storylink");
-for (var i = 0; i < scores.length; i++) {
-    var score = parseInt(scores[i].textContent.split(/\s/)[0]);
-    scores[i].style.fontWeight = 'bold';
-    scores[i].style.fontSize = '8pt';
-    if (score > max) {
-        score = max;
+for (var i = 0; i < subtexts.length; i++) {
+    var scoreEl = subtexts[i].querySelector(".score");
+    if (!scoreEl) {
+       continue;
     }
+    scoreEl.style.fontWeight = 'bold';
+    scoreEl.style.fontSize = '8pt';
+    var score = Math.min(parseInt(scoreEl.textContent.split(/\s/)[0]), max);
     var fontUnit = Math.round(score/divisionUnit);
     titles[i].style.fontSize = (10+(fontUnit*3)).toString()+'pt';
 }
