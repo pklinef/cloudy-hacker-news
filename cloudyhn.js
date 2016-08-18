f86761d7684f2edeb82968c5f1f7501fc30f633d
Phil Klinefelter <phillip.klinefelter@gmail.com>
2016-08-17 20:51:26 -0700

    Fixed change to score separator

--- cloudyhn.js
+++ cloudyhn.js
@@ -1,12 +1,14 @@
 var max = 300;
-var divisionUnit=max/10;
+var divisionUnit = max / 10;
 var scores = document.querySelectorAll(".score");
+var titles = document.querySelectorAll(".storylink");
 for (var i = 0; i < scores.length; i++) {
-    var score = scores[i].textContent.split(' ')[0];
+    var score = parseInt(scores[i].textContent.split(/\s/)[0]);
     scores[i].style.fontWeight = 'bold';
     scores[i].style.fontSize = '8pt';
-    if (score > max) { score = max; }
+    if (score > max) {
+        score = max;
+    }
     var fontUnit = Math.round(score/divisionUnit);
-    var title = scores[i].parentNode.parentNode.previousElementSibling.querySelectorAll("td.title a")[0];
-    title.style.fontSize = (10+(fontUnit*3)).toString()+'pt';
+    titles[i].style.fontSize = (10+(fontUnit*3)).toString()+'pt';
 }
